57c2493459228fb692b4bd06ce0ab6ca657e9e51
Juhyun Song <izen1231@gmail.com>
2024-08-08 23:10:16 +0900

    flutter version upgrade

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -34,7 +34,7 @@ class _MyAppState extends State<MyApp> {
 
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
   @override
   _MyHomePageState createState() => _MyHomePageState();
